2d4204844616d82c3cd1e591ada4b2eb3ba4787f
Pouya Lashkari <119332828+Pouya-L@users.noreply.github.com>
2023-12-09 15:46:58 +0000

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,3 @@
-//TIP Press <shortcut raw="SHIFT"/> twice to open the Search Everywhere dialog and type <b>show whitespaces</b>,
-// then press <shortcut raw="ENTER"/>. You can now see whitespace characters in your code.
 fun main() {
     exercise_12();
 }
